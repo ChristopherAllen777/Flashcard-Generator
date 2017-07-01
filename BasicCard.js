2acdb044df53fe3d987b7638f2cc6a2652bbbf14
Chris Allen <christopherallen777@gmail.com>
2017-07-01 13:51:20 -0500

    Finished Flashcard game/ added switch case function

--- BasicCard.js
+++ BasicCard.js
@@ -1,13 +1,4 @@
-// module.exports = BasicCard;
-
-// exports.BasicCard = function(text, cloze) {
-// 	var textToLower = text.toLowerCase();
-// 	var clozeToLower = cloze.toLowerCase();
-// 	this.full = text;
-// 	this.cloze = cloze;
-// 	blank = "";
-// 	for (var i = 0; i < cloze.length; i++) {
-//             blank += "_ " + "";
-// 	};
-// 	this.partial = text.replace(cloze, " " + blank);
-// }
+exports.BasicCard = function(front, back) {
+	this.front = front; // Contains text on front of card.
+	this.back = back; // Contains text on the back of the card.
+};
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,10 @@
 exports.ClozeCard = function(text, cloze) {
-	var textToLower = text.toLowerCase();
-	var clozeToLower = cloze.toLowerCase();
-	this.full = text;
-	this.cloze = cloze;
+	this.full = text; // Contains full text
+	this.cloze = cloze; // Contains the answer
 	blank = "";
 	for (var i = 0; i < cloze.length; i++) {
             blank += "_ " + "";
 	};
 	this.partial = text.replace(cloze, " " + blank);
 }
+
